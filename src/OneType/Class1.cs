cce920ca6354bba0e2156ed2a03d9c282aeb45ed
Mikhail Podolskiy <edge769@yandex.ru>
2020-05-01 22:41:03 +0300

    add Default Type Resolver

--- src/OneType/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace OneType
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/OneType/DefaultTypeResolver.cs
@@ -0,0 +1,25 @@
+﻿using OneType.Interface;
+using OneType.Interface.Models;
+using System;
+using System.Collections.Generic;
+
+namespace OneType
+{
+    public class DefaultTypeResolver : ITypeResolver
+    {
+        public int GetHashCode(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ObjectProperty> GetProperties(Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetValue(object obj, ObjectProperty property)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
