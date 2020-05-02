a42216968da21abe4543ac6a5f1d4a152fcdf412
Mikhail Podolskiy <edge769@yandex.ru>
2020-05-02 16:55:50 +0300

    add tests for DefaultTypeResolver GetProperties method

---
+++ src/OneType.Tests/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OneType.Tests.Models
+{
+    public struct Name
+    {
+        public Name(string firstName, string secondName)
+        {
+            FirstName = firstName;
+            SecondName = secondName;
+        }
+
+        public string FirstName { get; set; }
+
+        public string SecondName { get; set; }
+
+        public string FullName => $"{FirstName} {SecondName}";
+    }
+
+    public class User
+    {
+        public Name Name { get; set; }
+
+        public string Email { get; set; }
+
+        public int Age { get; set; }
+    }
+}
---
+++ src/OneType.Tests/Tests/DefaultTypeResolverTests.cs
@@ -0,0 +1,68 @@
+﻿using OneType.Interface.Models;
+using OneType.Tests.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace OneType.Tests.Tests
+{
+    public class DefaultTypeResolverTests
+    {
+        [Fact]
+        public void CanGetObjectProperties() =>
+            TestGetPropertiesMethod(ResultHasCorrectNumberOfProperties);
+
+        private readonly static GetPropertiesAssert ResultHasCorrectNumberOfProperties = new GetPropertiesAssert((expected, result) =>
+        {
+            Assert.NotNull(result);
+            Assert.Equal(expected.Count(), result.Count());
+        });
+
+        [Fact]
+        public void PropertiesHaveCorrectNames() =>
+            TestGetPropertiesMethod(ResultedPropertiesHaveCorrectNames);
+
+        private readonly static GetPropertiesAssert ResultedPropertiesHaveCorrectNames = new GetPropertiesAssert((expected, actual) =>
+        {
+            IOrderedEnumerable<string> getNamesOrdered(IEnumerable<ObjectProperty> objectProperties) =>
+                objectProperties.Select(p => p.Name)
+                    .OrderBy(pn => pn);
+
+            Assert.True(getNamesOrdered(expected).SequenceEqual(getNamesOrdered(actual)));
+        });
+
+        [Fact]
+        public void PropertiesHaveCorrectType() =>
+            TestGetPropertiesMethod(ResultedPropertiesHaveCorrectTypes);
+
+        private readonly static GetPropertiesAssert ResultedPropertiesHaveCorrectTypes = new GetPropertiesAssert((expected, actual) =>
+        {
+            IOrderedEnumerable<Type> getTypesOrdered(IEnumerable<ObjectProperty> objectProperties) =>
+                objectProperties.Select(p => p.Type)
+                    .OrderBy(pt => pt.Name);
+
+            Assert.True(getTypesOrdered(expected).SequenceEqual(getTypesOrdered(actual)));
+        });
+
+        private delegate void GetPropertiesAssert(IEnumerable<ObjectProperty> expected, IEnumerable<ObjectProperty> actual);
+        private void TestGetPropertiesMethod(GetPropertiesAssert assert)
+        {
+            // arrange
+            var testObj = new User();
+            var target = new DefaultTypeResolver();
+            var expectedProperties = new List<ObjectProperty>
+            {
+                new ObjectProperty(typeof(Name), nameof(User.Name)),
+                new ObjectProperty(typeof(string), nameof(User.Email)),
+                new ObjectProperty(typeof(int), nameof(User.Age))
+            };
+
+            // act
+            var result = target.GetProperties(testObj);
+
+            // assert
+            assert(expectedProperties, result);
+        }
+    }
+}
